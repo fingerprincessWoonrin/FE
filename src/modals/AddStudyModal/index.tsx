48a8bdcbea3c1469d972d2ffbc903748be7684a6
윤수용 <yunsuyong739@naver.com>
2023-12-20 10:04:28 +0900

    스터디그룹 기능 추가

--- src/modals/AddStudyModal/index.tsx
+++ src/modals/AddStudyModal/index.tsx
@@ -1,14 +1,34 @@
 import Portal from "../../portal";
 import * as S from "./style";
 import useModal from "../../hooks/useModal";
+import { useState } from "react"; 
 
-const AddFriendsModal = () => {
+const AddStydyModal = () => {
   const { closeModal } = useModal();
+  const [studyName, setStudyName] = useState(""); 
+
+  const handleCreateStudy = () => {
+    if (studyName.trim() !== "") {
+      const existingStudies = JSON.parse(localStorage.getItem("studies") ?? "[]") || [];
+
+      const newStudy = { name: studyName };
+      const updatedStudies = [...existingStudies, newStudy];
+
+      localStorage.setItem("studies", JSON.stringify(updatedStudies));
+
+      closeModal();
+    }
+  };
+
   return (
     <Portal onClose={closeModal}>
       <S.ModalContainer>
         <S.Title>스터디만들기</S.Title>
-        <S.SearchBar placeholder="추가할 친구 이름을 적으세요" />
+        <S.SearchBar
+          placeholder="스터디의 이름을 적으세요"
+          value={studyName} 
+          onChange={(e) => setStudyName(e.target.value)} 
+        />
         <S.AddStudyButton
           width="100%"
           height="3.5rem"
@@ -18,6 +38,7 @@ const AddFriendsModal = () => {
           fontSize="1.1rem"
           fontWeight="500"
           border="none"
+          onClick={handleCreateStudy} // 단계 2
         >
           새로운 스터디 만들기
         </S.AddStudyButton>
@@ -26,4 +47,4 @@ const AddFriendsModal = () => {
   );
 };
 
-export default AddFriendsModal;
+export default AddStydyModal;
