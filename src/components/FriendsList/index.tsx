bc29a0ed01d19182a00ad35aaf44b4b33b8d42ae
한준 <qw2661807@gmail.com>
2023-12-20 14:01:57 +0900

    친구 다시 리팩토링

--- src/components/FriendsList/index.tsx
+++ src/components/FriendsList/index.tsx
@@ -1,38 +1,28 @@
-import { useEffect, useState } from "react";
 import * as I from "../../assets";
-import { FriendObjectTypes } from "../../atoms";
 import * as S from "./style";
 
-interface Friend {
+interface FriendListProps {
   name: string;
+  onDelete?: () => void;
 }
-const FriendsList = () => {
-  const [friends, setFriends] = useState<Friend[]>([]);
-
-  useEffect(() => {
-    const storedFriends = JSON.parse(localStorage.getItem("friend") ?? "[]");
-    setFriends(storedFriends);
-  }, [localStorage.getItem("friend")]);
-
-  const handleDeleteStudy = (index: number) => {
-    const updatedStudies = [...friends];
-    updatedStudies.splice(index, 1);
-    setFriends(updatedStudies);
-    localStorage.setItem("friend", JSON.stringify(updatedStudies));
-  };
+const FriendsList: React.FC<FriendListProps> = ({ name, onDelete }) => {
   return (
     <>
-      {friends.map((item, idx) => (
-        <S.MemberListContainer key={idx}>
-          <S.Profile>
-            <I.profile width="50" height="50" />
-            <S.Name>{item.name}</S.Name>
-          </S.Profile>
-          <S.DeleteWrapper onClick={() => handleDeleteStudy(idx)}>
-            <I.deletes />
-          </S.DeleteWrapper>
-        </S.MemberListContainer>
-      ))}
+      <S.MemberListContainer>
+        <S.Profile>
+          <I.profile width="50" height="50" />
+          <S.Name>{name}</S.Name>
+        </S.Profile>
+        <S.DeleteWrapper
+          onClick={() => {
+            if (onDelete) {
+              onDelete();
+            }
+          }}
+        >
+          <I.deletes />
+        </S.DeleteWrapper>
+      </S.MemberListContainer>
     </>
   );
 };
