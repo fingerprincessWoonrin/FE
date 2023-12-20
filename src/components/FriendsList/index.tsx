196a783dbfbde7b51e6428c2e5831576f75ae80f
한준 <qw2661807@gmail.com>
2023-12-20 13:44:16 +0900

    친구 표시

--- src/components/FriendsList/index.tsx
+++ src/components/FriendsList/index.tsx
@@ -1,17 +1,25 @@
 import * as I from "../../assets";
+import { FriendObjectTypes } from "../../atoms";
 import * as S from "./style";
 
 const FriendsList = () => {
+  const storedArr = localStorage.getItem("friend");
+  const parseArr: FriendObjectTypes[] = storedArr ? JSON.parse(storedArr) : [];
+
   return (
-    <S.MemberListContainer>
-      <S.Profile>
-        <I.profile width="50" height="50" />
-        <S.Name>userName</S.Name>
-      </S.Profile>
-      <S.DeleteWrapper>
-        <I.deletes />
-      </S.DeleteWrapper>
-    </S.MemberListContainer>
+    <>
+      {parseArr.map((item, idx) => (
+        <S.MemberListContainer key={idx}>
+          <S.Profile>
+            <I.profile width="50" height="50" />
+            <S.Name>{item.name}</S.Name>
+          </S.Profile>
+          <S.DeleteWrapper>
+            <I.deletes />
+          </S.DeleteWrapper>
+        </S.MemberListContainer>
+      ))}
+    </>
   );
 };
 
