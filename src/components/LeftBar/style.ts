e94c348ad475b0a987978fafab72d620e92e0028
unrin <momlin069@gmail.com>
2023-12-20 16:48:55 +0900

    Leftbar auth 페이지에서 삭제

--- src/components/LeftBar/style.ts
+++ src/components/LeftBar/style.ts
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 
-export const LeftBarWrapper = styled.div`
+export const LeftBarWrapper = styled.div<{ pathname: string }>`
   position: fixed;
   left: 0;
   width: 6.3rem;
   height: 100%;
   background-color: #ffffff;
   box-shadow: 0px 2px 4px rgba(204, 204, 204, 0.25);
+  display: ${({ pathname }) =>
+    pathname.includes("login") || pathname.includes("signup")
+      ? "none"
+      : "block"};
 `;
 
 export const LeftBarContainer = styled.div`
